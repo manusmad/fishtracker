be285d18939c0e222280c7f60856d09748f6cefe
Manu Madhav <manusmad@gmail.com>
2016-07-05 10:21:13 -0400

    Combine tracks now chooses the points with maximum amplitude

--- frequencyTracking/helpers/combineTracks.m
+++ frequencyTracking/helpers/combineTracks.m
@@ -4,5 +4,18 @@ function handles = combineTracks(handles,ids)
     
     % Cannot have repeated times in the selection
     selectedIdx = find([handles.tracks.id]==ids(2));
-    [~,uniqueIdx,~] = unique([handles.tracks(selectedIdx).t]);
-    [handles.tracks(selectedIdx(uniqueIdx)).id] = deal(ids(1));
+%     [uniqueT,uniqueIdx,~] = unique([handles.tracks(selectedIdx).t]);
+    
+    t = [handles.tracks(selectedIdx).t];
+    a1 = [handles.tracks(selectedIdx).a1];
+    uT = unique(t);
+    
+%     uIdx = zeros(size(uT));
+    for tu = uT
+        tIdx = find(t==tu);
+        [~,maxIdx] = max(sum(a1(:,tIdx)));
+        handles.tracks(selectedIdx(tIdx(maxIdx))).id = ids(1);
+    end
+    
+    
+%     [handles.tracks(selectedIdx(uniqueIdx)).id] = deal(ids(1));
