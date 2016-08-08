063277cfb0bc866b5da326f9c9c9a43db4139ddb
Manu Madhav <manusmad@gmail.com>
2016-08-08 15:15:07 -0400

    First version of combined wilddata video generation code

--- paperfigs/wildData_videoGen.m
+++ paperfigs/wildData_videoGen.m
@@ -2,19 +2,37 @@
 addpath('../packages/addpath_recurse');
 addpath_recurse('..');
 
-%% Select datafolder - select the folder that contains the subfolders freqtracks,videotracks, raw etc
-[file_name,path_name]  = uigetfile({'*particle.mat'},'Select tracked wild file ...',pwd, ...
-                                    'MultiSelect', 'off');
+folder_name = uigetdir(pwd,'Select dataset folder ...');
 
-%%                                
-C = strsplit(path_name,filesep);
-baseFolder = strjoin(C(1:end-2),filesep);
-trackedVideo_dir_path   = fullfile(baseFolder,'tracked_video');
+% %% Select datafolder - select the folder that contains the subfolders freqtracks,videotracks, raw etc
+% [file_name,path_name]  = uigetfile({'*particle.mat'},'Select tracked wild file ...',pwd, ...
+%                                     'MultiSelect', 'off');
+            
+%% Choose which file to generate video for
+particle_file_name = 'TerraRonca_Calibration_01_100s_particle.mat';
+% particle_file_name = 'TerraRonca_Calibration_02_100s_particle.mat';
+% particle_file_name = 'TerraRonca_Calibration_03_100s_particle.mat';
+% particle_file_name = 'TerraRonca_Calibration_04_100s_particle.mat';
 
-trackedVideo_file_name  = strrep(file_name,'particle','video');
+tracked_dir_path        = fullfile(folder_name,'terraronca','tracked');
+trackedVideo_dir_path   = fullfile(folder_name,'terraronca','tracked_video');
+spec_dir_path           = fullfile(folder_name,'terraronca','spec');
+freqtracks_dir_path     = fullfile(folder_name,'terraronca','freqtracks');
+
+trackedVideo_file_name  = strrep(particle_file_name,'particle','video');
+spec_file_name          = strrep(particle_file_name,'particle','spec');
+freqtracks_file_name    = strrep(particle_file_name,'particle','tracks');
+
+
 %% Load files
     
-particleTracked         = load(fullfile(path_name, file_name),'xMean', 'yMean','thMean','gridCoord','tankCoord','nFish','ampAll');
+particleTracked         = load(fullfile(tracked_dir_path, particle_file_name),'xMean', 'yMean','thMean','gridCoord','tankCoord','nFish','ampAll','fishTime');
+spec                    = load(fullfile(spec_dir_path, spec_file_name),'spec');
+freqtracks              = load(fullfile(freqtracks_dir_path, freqtracks_file_name),'tracks');
+
+spec = hlp_deserialize(spec.spec);
+Smag = mean(normSpecMag(spec.S),3);
+
 
 %% Video parameters
 
@@ -22,6 +40,7 @@ nFrame                  = size(particleTracked.thMean,2);
 frameInterval           = 767:962;
 frameRate               = 8;
 fishSelect              = [9 10];
+nFrames                 = frameInterval(end)-frameInterval(1)+1;
 
 %%
 fW                  = 1;
@@ -33,16 +52,39 @@ set(gcf,'color',[0 0 0]);
 
 writerObj           = VideoWriter(fullfile(trackedVideo_dir_path, trackedVideo_file_name),'MPEG-4');
 writerObj.FrameRate = frameRate;
+
+progressbar('Writing Video');
 open(writerObj);  
 
 flag = zeros(1,particleTracked.nFish);
 dispStep = ones(1,particleTracked.nFish);
-for frameLoop = 1:length(frameInterval)
-    hold on;
+for frameIdx = frameInterval
+    progressbar((frameIdx-frameInterval(1))/nFrames);
+
+    time = particleTracked.fishTime(frameIdx);
+    
+    figure(hAxis), cla;
+    
+    subplot(1,2,1), hold on;
+
+    [~,hSpec] = plotSpectrogram(gca,spec.T,spec.F,Smag);
+    [~,hTracks] = plotTracks(gca,freqtracks.tracks,[]);
+
+    ylimits = [290,450];
+    plot([time,time],ylimits,'--y','LineWidth',1);
+
+    ylim(ylimits);
+    xlim([spec.T(1),spec.T(end)]);
+
+    xlabel('Time (s)');
+    ylabel('Frequency (Hz)');
+    
+    hold off;
+    
+    subplot(1,2,2), hold on;
     plot(particleTracked.gridCoord(:,1),particleTracked.gridCoord(:,2),'ok','LineWidth',3.01);
     plot(particleTracked.gridCoord(:,1),particleTracked.gridCoord(:,2),'+k','LineWidth',3.01);
     
-    frameIdx = frameInterval(frameLoop);       
     for i = 1:length(fishSelect)
         fID = fishSelect(i);
         if sum(~isnan(squeeze(particleTracked.ampAll(fID,:,frameIdx))))
@@ -63,8 +105,10 @@ for frameLoop = 1:length(frameInterval)
     ylim([-125 125])
     f = getframe(hAxis);
     writeVideo(writerObj,f);
-    cla(hAxis)
+
 end
 close(writerObj);
+progressbar(1);
+
 display('Done')
 
