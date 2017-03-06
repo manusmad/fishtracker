db1e4f7e8a4ebca0f84cc22d6cfa0774fe876d7f
Ravi J <rperurj1@jhu.edu>
2017-03-06 10:35:57 -0500

    removed unnecessary files from spatial tracking folder for NatMCode

---
+++ spatialTracking/FS_proc_noise.m
@@ -0,0 +1,20 @@
+function sysv_noise = FS_proc_noise(rw, clmn, motion, tInt)
+
+% Generating process noise
+
+sig_x   = (tInt/3.02)^0.0001;
+sig_y   = (tInt/3.02)^0.0001;
+sig_th  = (tInt*pi/(.5));
+sig_z   = (tInt/3.02)^0.0001;
+
+sig_pos         = [sig_x; sig_y;sig_th];
+sig_pos3D       = [sig_x; sig_y;sig_th;sig_z];
+
+if strcmp(motion, 'random')
+    sig_u = sig_pos;
+elseif strcmp(motion, 'random3D')
+    sig_u = sig_pos3D;
+end
+
+sig_mean    = zeros(rw,1);
+sysv_noise  = [normrnd(repmat(sig_mean,1,clmn), repmat(sig_u,1,clmn), rw,clmn); zeros(1,clmn)];
